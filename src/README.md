5aba6f2250c722e44e63a5d5317999a79ac842da
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-11-26 09:58:32 +0530

    Solve Bricks Game

--- src/README.md
+++ src/README.md
@@ -6,6 +6,7 @@
 - [Tutorial](https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/tutorial/)
 - [Zoos](https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/is-zoo-f6f309e7/)
 - [e-maze-in](https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/e-maze-in-1aa4e2ac/)
+- [Bricks Game](https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/bricks-game-5140869d/)
 ### Complexity Analysis
 #### Time and Space Complexity
 - [A + B](https://www.hackerearth.com/practice/basic-programming/complexity-analysis/time-and-space-complexity/practice-problems/algorithm/a-b-4/)
---
+++ src/www/hackerearth/com/practice/basic_programming/input_output/basics_of_input_output/practice_problems/bricks_game/TestClass.java
@@ -0,0 +1,38 @@
+// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/bricks-game-5140869d/
+
+package www.hackerearth.com.practice.basic_programming.input_output.basics_of_input_output.practice_problems.bricks_game;
+
+import java.util.*;
+
+class TestClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        int n = sc.nextInt();
+        int p = 0, m = 0;
+        String result = "";
+
+        while (true) {
+            p = i;
+            m = i * 2;
+            ++i;
+
+            if (p >= n) {
+                result = "Patlu";
+                break;
+            } else {
+                n -= p;
+            }
+
+            if (m >= n) {
+                result = "Motu";
+                break;
+            } else {
+                n -= m;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
