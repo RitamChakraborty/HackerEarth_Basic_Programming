c3e5ffb4431e56fc0592382053aadd7e414408ba
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-12-07 19:07:52 +0530

    Optimize Seven-Segment Display

--- src/www/hackerearth/com/practice/basic_programming/input_output/basics_of_input_output/practice_problems/seven_segment_display/TestClass.java
+++ src/www/hackerearth/com/practice/basic_programming/input_output/basics_of_input_output/practice_problems/seven_segment_display/TestClass.java
@@ -8,21 +8,23 @@ import java.io.*;
 class TestClass {
 	private static int matchNumber(int num) {
 		switch (num) {
-			case 0: return 6;
+			case 0:
+			case 6:
+			case 9:
+				return 6;
 			case 1: return 2;
-			case 2: return 5;
-			case 3: return 5;
+			case 2:
+			case 3:
+			case 5:
+				return 5;
 			case 4: return 4;
-			case 5: return 5;
-			case 6: return 6;
 			case 7: return 3;
 			case 8: return 7;
-			case 9: return 6;
 			default: return 0;
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 		
 		int t = sc.nextInt();
