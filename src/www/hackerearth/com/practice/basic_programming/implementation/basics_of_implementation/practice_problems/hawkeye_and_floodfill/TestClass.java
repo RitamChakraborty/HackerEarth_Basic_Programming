ab0139013d1aa0d90797b3db5fb08b255fae24e1
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-12-14 14:00:58 +0530

    Try Hawkeye and FloodFill

---
+++ src/www/hackerearth/com/practice/basic_programming/implementation/basics_of_implementation/practice_problems/hawkeye_and_floodfill/TestClass.java
@@ -0,0 +1,59 @@
+// FIXME: 12/14/19 
+// https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/hawkeye-and-floodfill/
+
+package www.hackerearth.com.practice.basic_programming.implementation.basics_of_implementation.practice_problems.hawkeye_and_floodfill;
+
+import java.io.*;
+import java.util.*;
+
+public class TestClass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		int n = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int p;
+		int[][] arr = new int[n][n];
+		
+		for (int i = a; i >= 0; --i) {
+			arr[i][b] = i;
+		}
+		
+		p = a;
+		for (int i = a; i < n; ++i) {
+			arr[i][b] = p > 0 ? p-- : 0;
+		}
+		
+		for (int i = 0; i < n; ++i) {
+			int max = 0;
+			int index = 0;
+			
+			for (int j = 0; j < n; ++j) {
+				if (arr[i][j] > max) {
+					max = arr[i][j];
+					index = j;
+				}
+			}
+			
+			p = max;
+			for (int j = index; j >= 0; --j) {
+				arr[i][j] = p > 0 ? p-- : 0;
+			}
+			
+			p = max;
+			for (int j = index; j < n; ++j) {
+				arr[i][j] = p > 0 ? p-- : 0;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder(" ");
+		for (int i = 0; i < n; ++i) {
+			sb.append("\n");
+			for (int j = 0; j < n; ++j) {
+				sb.append(String.valueOf(arr[i][j])).append(" ");
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
